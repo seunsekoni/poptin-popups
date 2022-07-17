7c4ad9b5f17706c2bee6d18940ae5d38bbfec8aa
Sekoni Oluwaseun <sekonioluwaseun@gmail.com>
2022-07-17 16:10:01 +0100

    fix `Specified key was too long` error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
@@ -29,5 +30,7 @@ class AppServiceProvider extends ServiceProvider
 
             URL::forceScheme('https');
         }
+
+        Schema::defaultStringLength(191);
     }
 }
