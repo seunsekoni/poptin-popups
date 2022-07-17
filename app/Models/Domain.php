8e00d93279596ebf638b20db004844008ebc0b93
Sekoni Oluwaseun <sekonioluwaseun@gmail.com>
2022-07-17 16:33:56 +0100

    add mutators for url

--- app/Models/Domain.php
+++ app/Models/Domain.php
@@ -11,6 +11,19 @@ class Domain extends Model
     use HasFactory;
 
     /**
+     * Interact with the popup's url.
+     *
+     * @param  string  $value
+     * @return \Illuminate\Database\Eloquent\Casts\Attribute
+     */
+    protected function topLevel(): Attribute
+    {
+        return Attribute::make(
+            set: fn ($value) => preg_replace('/^(https?:|http?:)\/\//', '', $value),
+        );
+    }
+
+    /**
      * Get the owning user model.
      *
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
