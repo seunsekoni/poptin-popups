079d63827ee991ad7ab2aadae00d02d83ead883c
Sekoni Oluwaseun <sekonioluwaseun@gmail.com>
2022-07-17 12:22:40 +0100

    switch to eager loading

--- resources/views/popups/index.blade.php
+++ resources/views/popups/index.blade.php
@@ -37,7 +37,7 @@
             </tr>
         </thead>
         <tbody>
-            @foreach ($popups as $popup)
+            @foreach ($domain->popups as $popup)
                 <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
                     <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                         {{  $popup->page }}
@@ -67,6 +67,3 @@
 </div>
 
 @endsection
-
-<!-- Delete Modal -->
-<x-delete-confirmation />
